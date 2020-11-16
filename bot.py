62ac182284718a1278de49ee4777b114156e43d6
binoverfl0w <janimarku20@gmail.com>
2020-11-16 16:07:15 +0100

    Added Misc Cog, non-thumbnail posts

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 
 from discord.ext import commands
 from dotenv import load_dotenv
+import discord
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,8 +12,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='&')
 bot.load_extension('cog')
 
+
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game("m'shkatrrut jeten"))
     print(f'{bot.user.name} has connected to Discord!')
 
 bot.run(TOKEN)
--- cog.py
+++ cog.py
@@ -6,6 +6,7 @@ class Reddit(commands.Cog):
 
     def __init__(self,bot):
         self.bot = bot
+        self.visited = {}
 
     @commands.command(name="subscribe",aliases=['sub'],help="Subscribe to subreddit.")
     async def subscribe(self,ctx, name_of_subreddit: str = None, channel: discord.TextChannel = None):
@@ -24,25 +25,52 @@ class Reddit(commands.Cog):
             await ctx.send("You need to write the name of subreddit. See &help reddit.")
             return
         else:
-            r = requests.get(f"https://www.reddit.com/r/{name_of_subreddit}/hot.json?sort=hot&limit=4",headers = {'User-agent': 'Haberet'})
+            r = requests.get(f"https://www.reddit.com/r/{name_of_subreddit}/hot.json?sort=hot",headers = {'User-agent': 'Haberet'})
             data = r.json()
             if 'error' in data:
                 await ctx.send(f"Error {data['error']} occured.")
                 return
+            if not name_of_subreddit in self.visited:
+                self.visited[name_of_subreddit] = []
             for obj in data['data']['children']:
-                if obj['data']['pinned'] == False:
-                    embed = discord.Embed(title=obj['data']['title'])
-                    if 'thumbnail' in obj['data']:
-                        if obj['data']['over_18'] == True and ctx.message.channel.nsfw == False:
-                            await ctx.send("Jo nsfw se nuk do Dela.")
-                            return
-                        if obj['data']['url'].endswith(".jpg") or obj['data']['url'].endswith(".png") or obj['data']['url'].endswith(".gif"):
-                            embed.set_image(url=obj['data']['url'])
-                            await ctx.send(embed=embed)
-                            return
-        response = "Ik o rrk gjej shoket <:lod:773579563182981120>"
+                if obj['data']['stickied'] == True:
+                    continue
+                if 'permalink' in obj['data']:
+                    if obj['data']['permalink'] in self.visited[name_of_subreddit]:
+                        print(self.visited[name_of_subreddit])
+                        continue
+                    else:
+                        self.visited[name_of_subreddit].append(obj['data']['permalink'])
+
+                embed = discord.Embed(title=obj['data']['title'])
+                if 'url' in obj['data']:
+                        # if obj['data']['over_18'] == True and ctx.message.channel.nsfw == False:
+                        #     await ctx.send("Jo nsfw se nuk do Dela.")
+                        #     return
+                    if obj['data']['url'].endswith(".jpg") or obj['data']['url'].endswith(".png") or obj['data']['url'].endswith(".gif"):
+                        embed.set_image(url=obj['data']['url'])
+                        await ctx.send(embed=embed)
+                    elif 'selftext' in obj['data']:
+                        embed.add_field(name="...",value=obj['data']['selftext'],inline=False)
+                        await ctx.send(embed=embed)
+                    return
+        response = "Ti s'di ca flet o shok <:lod:773579563182981120>"
         await ctx.send(response)
+
+class Misc(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(name="mbeshtet",help="Mbeshtet nje shok/shoqe ne nje situate")
+    async def mbeshtet(self,ctx,user:discord.User=None):
+        await ctx.send(f"vari m 🍆 m ❤ {user.mention}")
+
+    @commands.command(name="sqaroje", help="Sqaroje shokun/shoqen se ca e gjen")
+    async def sqaroje(self,ctx,user:discord.User=None):
+        await ctx.send(f"ULI DURET O RIGELS {user.mention}")
 
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
+    bot.add_cog(Misc(bot))
